fd2a7db6f58aab1de194b7623c0d699d4706867b
dhuw <hazlewood.dyl@gmail.com>
2022-09-18 11:42:54 -0700

    changing user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
-const userRoutes = require("./user-routes");
-const thoughtRoutes = require("./thought-routes");
 
-router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
-
-module.exports = router;
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
